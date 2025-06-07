d95890085a2c3e7a61ff2848a53c05e24a458c73
User <User@DESKTOP-GAB3BFD>
2025-06-07 20:15:01 +0200

    populated event type table with values

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿SET IDENTITY_INSERT EventTypeModel ON;
+
+INSERT INTO EventTypeModel (EventTypeId,EventType )   
+VALUES ('1','Corporate'),
+       ('2','Wedding'),
+       ('3','Party'),
+       ('4','Funeral'),
+      ('5','Other');
+
+SET IDENTITY_INSERT EventTypeModel OFF;
